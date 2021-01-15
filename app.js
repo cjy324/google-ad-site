52b39249f6614d462f78f9f2e8fa458c3ca8c468
cjy324 <juy32400@naver.com>
2021-01-15 16:10:19 +0900

    21.01.15 배포

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+/* 메뉴바 토글-아이콘 애니메이션 시작 */
+function MobileTopBar__init() {
+  $('.menu-bar__status-icon').click(function() {
+    let $this = $(this);
+    
+    if ( $this.hasClass('active') ) {
+      $this.removeClass('active');
+    }
+    else {
+      $this.addClass('active');
+    }
+  });
+}
+
+MobileTopBar__init();
+
+/* 메뉴바 토글-아이콘 애니메이션 끝 */
+
+
+/* 메뉴바 토글-메뉴 나오기 시작 */ 
+$(document).ready(function(){
+        $(".menu-bar__status-icon").click(function(){
+          var submenu = $(this).next("div");
+
+       if( submenu.is(":visible") ){
+                submenu.slideUp();
+            }else{
+                submenu.slideDown();
+            }
+
+        });
+    });
+// https://stove99.tistory.com/103
+/* 메뉴바 토글-메뉴 나오기 끝 */ 
+
+
+
+
+function ArticleDetail__Body__init() {
+	if (toastui === undefined) {
+		return;
+	}
+
 
 /* 유튜브 함수 시작 */
 
@@ -22,26 +65,53 @@
 		el.innerHTML = `<div class="toast-ui-youtube-plugin-wrap"><iframe src="https://www.youtube.com/embed/${youtubeId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
 	}
 	/* 유튜브 함수 끝 */
+	
+	
+	
+	/* codepen 함수 시작 */
+function codepenPlugin() {
+  toastui.Editor.codeBlockManager.setReplacer('codepen', url => {
+    const wrapperId = `yt${Math.random().toString(36).substr(2, 10)}`;
+
+    // Avoid sanitizing iframe tag
+    setTimeout(renderCodepen.bind(null, wrapperId, url), 0);
+
+    return `<div id="${wrapperId}"></div>`;
+  });
+}
+
+function renderCodepen(wrapperId, url) {
+  const el = document.querySelector(`#${wrapperId}`);
+  
+  var urlParams = new URLSearchParams(url.split('?')[1]);
+  var height = urlParams.get('height');
+
+  el.innerHTML = `<div class="toast-ui-codepen-plugin-wrap"><iframe height="${height}" scrolling="no" src="${url}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe></div>`;
+}
+/* codepen 함수 끝 */
+	
+	
 
 
 /* toastui-viewr 함수 시작 */
+function EditorViewer__init() {
+	  $('.toast-ui-viewer').each(function(index, node) {
+	    var initialValue = $(node).prev().html().trim().replace(/t-script/gi, 'script');
 
-function ArticleDetail__Body__init() {
-	if (toastui === undefined) {
-		return;
-	}
-
-	var body = document.querySelector('.article-detail-cell__body > div > span');
-	var initValue = body.innerHTML.trim();
+//	var body = document.querySelector('.article-detail-cell__body > div > span');
+//	var initValue = body.innerHTML.trim();
 
 	
 	var viewer = new toastui.Editor.factory({
-		el: body,
-		initialValue: initValue,
+		el: node,
+		initialValue: initialValue,
 		viewer: true,
-		plugins: [toastui.Editor.plugin.codeSyntaxHighlight, youtubePlugin]
+		plugins: [toastui.Editor.plugin.codeSyntaxHighlight, youtubePlugin, codepenPlugin]
 	});
-};
+});
+}
+	EditorViewer__init();
+}
 
 ArticleDetail__Body__init();
 
@@ -71,5 +141,3 @@ $(function() {
 });
 
 /* top-button 옵션 끝 */
-
-
